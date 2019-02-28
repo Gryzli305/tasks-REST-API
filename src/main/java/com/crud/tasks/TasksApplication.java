65f25f62dd0f653b40ecb89258b93d03701e651f
unknown <gzietek@tlen.pl>
2019-02-28 19:23:45 +0100

    spring fail 2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,15 +1,11 @@
 package com.crud.tasks;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-import org.springframework.test.context.junit4.SpringRunner;
-
 //public class TasksApplication extends SpringBootServletInitializer {
-//@RunWith(SpringRunner.class)
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
 
@@ -17,12 +13,9 @@ public class TasksApplication extends SpringBootServletInitializer {
 
         SpringApplication.run(TasksApplication.class, args);
     }
-
-
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //        return application.sources(TasksApplication.class);
 //    }
-
 }
 
